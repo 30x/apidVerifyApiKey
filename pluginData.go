539c4e3602b01dbfeb91b91bf0c624b31eb77b4c
Haoming Zhang <zhm1993zhm@gmail.com>
2017-04-11 10:48:11 -0700

    fixed XAPID-862, make sure we log things properly, format code

--- pluginData.go
+++ pluginData.go
@@ -3,7 +3,7 @@ package apidVerifyApiKey
 import "github.com/30x/apid-core"
 
 var pluginData = apid.PluginData{
-	Name: "apidVerifyAPIKey",
+	Name:    "apidVerifyAPIKey",
 	Version: "0.0.2",
 	ExtraData: map[string]interface{}{
 		"schemaVersion": "0.0.2",
