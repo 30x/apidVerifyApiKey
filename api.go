604a75ea501455db8727c7b6375a4d566311395c
Rajanish GJ <rajanishgj@google.com>
2017-08-01 13:16:43 -0700

    [XAPID-1037] refactor verify apikey api impl

--- api.go
+++ api.go
@@ -103,6 +103,155 @@ func verifyAPIKey(verifyApiKeyReq VerifyApiKeyRequest, db apid.DB) ([]byte, erro
 	path := verifyApiKeyReq.UriPath
 	//action := verifyApiKeyReq.Action
 
+	/* these fields need to be nullable types for scanning.  This is because when using json snapshots,
+	   and therefore being responsible for inserts, we were able to default everything to be not null.  With
+	   sqlite snapshots, we are not necessarily guaranteed that
+	*/
+	var finalDeveloperDetails DeveloperDetails
+	var companyDetails CompanyDetails
+	var proxies, environments string
+	var resName, resEnv, cType, tenantId sql.NullString
+
+	tempDeveloperDetails := DeveloperDetails{}
+	appDetails := AppDetails{}
+	apiProductDetails := ApiProductDetails{}
+	clientIdDetails := ClientIdDetails{}
+	clientIdDetails.ClientId = key
+
+	err := getApiKeyDetails(db, verifyApiKeyReq, &proxies, &environments, &resName, &resEnv, &cType, &tenantId, &tempDeveloperDetails, &appDetails, &apiProductDetails,&clientIdDetails)
+
+	switch {
+	case err == sql.ErrNoRows:
+		reason := "API Key verify failed for (" + key + ", " + organizationName +")"
+		errorCode := "REQ_ENTRY_NOT_FOUND"
+		return errorResponse(reason, errorCode)
+
+	case err != nil:
+		reason := err.Error()
+		errorCode := "SEARCH_INTERNAL_ERROR"
+		return errorResponse(reason, errorCode)
+	}
+
+	/*
+	 * Perform all validations related to the Query made with the data
+	 * we just retrieved
+	 */
+	result := validatePath(resName.String, path)
+	if result == false {
+		reason := "Path Validation Failed (" + resName.String + " vs " + path + ")"
+		errorCode := "PATH_VALIDATION_FAILED"
+		return errorResponse(reason, errorCode)
+
+	}
+
+	/* Verify if the ENV matches */
+	result = validateEnv(resEnv.String, environmentName)
+	if result == false {
+		reason := "ENV Validation Failed (" + resEnv.String + " vs " + environmentName + ")"
+		errorCode := "ENV_VALIDATION_FAILED"
+		return errorResponse(reason, errorCode)
+	}
+
+	setAttributes(db, tenantId.String, &clientIdDetails, &appDetails, &tempDeveloperDetails, &apiProductDetails)
+
+	if appDetails.CallbackUrl != "" {
+		clientIdDetails.RedirectURIs = []string{appDetails.CallbackUrl}
+	}
+	if err := json.Unmarshal([]byte(proxies), &apiProductDetails.Apiproxies); err != nil {
+		log.Debug("unmarshall error for proxies, sending as is ", err)
+		apiProductDetails.Apiproxies = []string{proxies }
+	}
+	if err := json.Unmarshal([]byte(environments), &apiProductDetails.Environments); err != nil {
+		log.Debug("unmarshall error for proxies, sending as is ", err)
+		apiProductDetails.Environments = []string{environments }
+	}
+
+	if cType.String == "developer" {
+		finalDeveloperDetails = tempDeveloperDetails
+	} else {
+		companyDetails = CompanyDetails{
+			Id: tempDeveloperDetails.Id,
+			DisplayName: tempDeveloperDetails.UserName,
+			Status: tempDeveloperDetails.Status,
+			CreatedAt: tempDeveloperDetails.CreatedAt,
+			CreatedBy: tempDeveloperDetails.CreatedBy,
+			LastmodifiedAt: tempDeveloperDetails.LastmodifiedAt,
+			LastmodifiedBy: tempDeveloperDetails.LastmodifiedBy,
+			Attributes: tempDeveloperDetails.Attributes,
+		}
+
+	}
+
+	resp := VerifyApiKeySuccessResponse{
+		ClientId: clientIdDetails,
+		Organization: organizationName,
+		Environment: resEnv.String,
+		Developer:   finalDeveloperDetails,
+		Company:     companyDetails,
+		App:         appDetails,
+		ApiProduct:  apiProductDetails,
+		// Identifier of the authorization code. This will be unique for each request.
+		Identifier: key, // TODO : what is this ?????
+		Kind:       "Collection", // TODO : what is this ????
+
+	}
+
+	return json.Marshal(resp)
+}
+
+func setAttributes(db apid.DB, tenantId string, clientIdDetails *ClientIdDetails, appDetails *AppDetails, tempDeveloperDetails *DeveloperDetails, apiProductDetails *ApiProductDetails ){
+	clientIdAttributes := getKmsAttributes(db, tenantId, clientIdDetails.ClientId)
+	developerAttributes := getKmsAttributes(db, tenantId, tempDeveloperDetails.Id)
+	appAttributes := getKmsAttributes(db, tenantId, appDetails.Id)
+	apiProductAttributes := getKmsAttributes(db, tenantId, apiProductDetails.Id)
+
+	clientIdDetails.Attributes = clientIdAttributes
+	appDetails.Attributes = appAttributes
+	apiProductDetails.Attributes = apiProductAttributes
+	tempDeveloperDetails.Attributes = developerAttributes
+}
+func getKmsAttributes(db apid.DB, tenantId string, entityId string) []Attribute {
+	sql := "select name, value from kms_attributes where tenant_id = $1 and entity_id = $2"
+	attributesForQuery := []Attribute{}
+	attributes, err := db.Query(sql, tenantId, entityId)
+
+
+	if err != nil {
+		log.Error("Error while fetching attributes for tenant id : %s and entityId : %s", tenantId, entityId, err)
+		return attributesForQuery
+	}
+	for attributes.Next() {
+		att := Attribute{}
+		err := attributes.Scan(
+			&att.Name,
+			&att.Value,
+		)
+		if err != nil {
+			log.Error("error fetching attributes for entityid ", entityId , err)
+		}
+		if att.Name != "" {
+			attributesForQuery = append(attributesForQuery, att)
+		}
+	}
+	log.Debug("attributes returned for query ", sql , " are ", attributesForQuery , tenantId , entityId)
+	return attributesForQuery
+}
+
+func errorResponse(reason, errorCode string) ([]byte, error) {
+	if errorCode == "SEARCH_INTERNAL_ERROR" {
+		log.Error(reason)
+	} else {
+		log.Debug(reason)
+	}
+	resp := ErrorResponse{
+		ResponseCode:    errorCode,
+		ResponseMessage: reason,
+	}
+	return json.Marshal(resp)
+}
+
+func getApiKeyDetails(db apid.DB, verifyApiKeyReq VerifyApiKeyRequest, proxies, environments *string, resName, resEnv, cType, tenantId *sql.NullString, tempDeveloperDetails *DeveloperDetails, appDetails *AppDetails, apiProductDetails *ApiProductDetails, clientIdDetails *ClientIdDetails) (error) {
+
 	sSql := `
 		SELECT
 			ap.api_resources,
@@ -238,184 +387,52 @@ func verifyAPIKey(verifyApiKeyReq VerifyApiKeyRequest, db apid.DB) ([]byte, erro
 			AND o.name = $2)
 	;`
 
-	/* these fields need to be nullable types for scanning.  This is because when using json snapshots,
-	   and therefore being responsible for inserts, we were able to default everything to be not null.  With
-	   sqlite snapshots, we are not necessarily guaranteed that
-	*/
-	var finalDeveloperDetails DeveloperDetails
-	var proxies, environments string
-	var resName, resEnv, cType, tenantId sql.NullString
-
-	tempDeveloperDetails := DeveloperDetails{}
-	companyDetails := CompanyDetails{}
-	appDetails := AppDetails{}
-	apiProductDetails := ApiProductDetails{}
-	clientIdDetails := ClientIdDetails{}
-	clientIdDetails.ClientId = key
-
-	err := db.QueryRow(sSql, key, organizationName).
+	err := db.QueryRow(sSql, verifyApiKeyReq.Key, verifyApiKeyReq.OrganizationName).
 		Scan(
-			&resName,
-			&resEnv,
-			&cType,
-			&tenantId,
-			&clientIdDetails.Status,
-			&clientIdDetails.ClientSecret,
+		resName,
+		resEnv,
+		cType,
+		tenantId,
+		&clientIdDetails.Status,
+		&clientIdDetails.ClientSecret,
 
-			&tempDeveloperDetails.Id,
-			&tempDeveloperDetails.UserName,
-			&tempDeveloperDetails.FirstName,
-			&tempDeveloperDetails.LastName,
-			&tempDeveloperDetails.Email,
-			&tempDeveloperDetails.Status,
-			&tempDeveloperDetails.CreatedAt,
-			&tempDeveloperDetails.CreatedBy,
-			&tempDeveloperDetails.LastmodifiedAt,
-			&tempDeveloperDetails.LastmodifiedBy,
+		&tempDeveloperDetails.Id,
+		&tempDeveloperDetails.UserName,
+		&tempDeveloperDetails.FirstName,
+		&tempDeveloperDetails.LastName,
+		&tempDeveloperDetails.Email,
+		&tempDeveloperDetails.Status,
+		&tempDeveloperDetails.CreatedAt,
+		&tempDeveloperDetails.CreatedBy,
+		&tempDeveloperDetails.LastmodifiedAt,
+		&tempDeveloperDetails.LastmodifiedBy,
 
-			&appDetails.Id,
-			&appDetails.Name,
-			&appDetails.AccessType,
-			&appDetails.CallbackUrl,
-			&appDetails.DisplayName,
-			&appDetails.Status,
-			&appDetails.AppFamily,
-			&appDetails.Company,
-			&appDetails.CreatedAt,
-			&appDetails.CreatedBy,
-			&appDetails.LastmodifiedAt,
-			&appDetails.LastmodifiedBy,
+		&appDetails.Id,
+		&appDetails.Name,
+		&appDetails.AccessType,
+		&appDetails.CallbackUrl,
+		&appDetails.DisplayName,
+		&appDetails.Status,
+		&appDetails.AppFamily,
+		&appDetails.Company,
+		&appDetails.CreatedAt,
+		&appDetails.CreatedBy,
+		&appDetails.LastmodifiedAt,
+		&appDetails.LastmodifiedBy,
 
-			&apiProductDetails.Id,
-			&apiProductDetails.Name,
-			&apiProductDetails.DisplayName,
-			&apiProductDetails.QuotaLimit,
-			&apiProductDetails.QuotaInterval,
-			&apiProductDetails.QuotaTimeunit,
-			&apiProductDetails.CreatedAt,
-			&apiProductDetails.CreatedBy,
-			&apiProductDetails.LastmodifiedAt,
-			&apiProductDetails.LastmodifiedBy,
-			&proxies,
-			&environments,
-		)
-	switch {
-		case err == sql.ErrNoRows:
-			reason := "API Key verify failed for (" + key + ", " + organizationName + ", " + path + ")"
-			errorCode := "REQ_ENTRY_NOT_FOUND"
-			return errorResponse(reason, errorCode)
+		&apiProductDetails.Id,
+		&apiProductDetails.Name,
+		&apiProductDetails.DisplayName,
+		&apiProductDetails.QuotaLimit,
+		&apiProductDetails.QuotaInterval,
+		&apiProductDetails.QuotaTimeunit,
+		&apiProductDetails.CreatedAt,
+		&apiProductDetails.CreatedBy,
+		&apiProductDetails.LastmodifiedAt,
+		&apiProductDetails.LastmodifiedBy,
+		proxies,
+		environments,
+	)
 
-		case err != nil:
-			reason := err.Error()
-			errorCode := "SEARCH_INTERNAL_ERROR"
-			return errorResponse(reason, errorCode)
-	}
-
-
-	/*
-	 * Perform all validations related to the Query made with the data
-	 * we just retrieved
-	 */
-	result := validatePath(resName.String, path)
-	if result == false {
-		reason := "Path Validation Failed (" + resName.String + " vs " + path + ")"
-		errorCode := "PATH_VALIDATION_FAILED"
-		return errorResponse(reason, errorCode)
-
-	}
-
-	/* Verify if the ENV matches */
-	result = validateEnv(resEnv.String, environmentName)
-	if result == false {
-		reason := "ENV Validation Failed (" + resEnv.String + " vs " + environmentName + ")"
-		errorCode := "ENV_VALIDATION_FAILED"
-		return errorResponse(reason, errorCode)
-	}
-
-	clientIdAttributes := getKmsAttributes(db, tenantId.String, clientIdDetails.ClientId)
-	appAttributes := getKmsAttributes(db, tenantId.String, appDetails.Id)
-	apiProductAttributes := getKmsAttributes(db, tenantId.String, apiProductDetails.Id)
-	developerAttributes := getKmsAttributes(db, tenantId.String, tempDeveloperDetails.Id)
-
-	clientIdDetails.Attributes = clientIdAttributes
-	appDetails.Attributes = appAttributes
-	apiProductDetails.Attributes = apiProductAttributes
-
-	if appDetails.CallbackUrl != "" {
-		clientIdDetails.RedirectURIs = []string{appDetails.CallbackUrl}
-	}
-	if err := json.Unmarshal([]byte(proxies), &apiProductDetails.Apiproxies); err != nil {
-		log.Debug("unmarshall error for proxies, sending as is ", err)
-		apiProductDetails.Apiproxies = []string{proxies }
-	}
-	if err := json.Unmarshal([]byte(environments), &apiProductDetails.Environments); err != nil {
-		log.Debug("unmarshall error for proxies, sending as is ", err)
-		apiProductDetails.Environments = []string{environments }
-	}
-
-	if cType.String == "developer" {
-		finalDeveloperDetails := &tempDeveloperDetails
-		finalDeveloperDetails.Attributes = developerAttributes
-	} else {
-		companyDetails := CompanyDetails{
-			Id: tempDeveloperDetails.Id,
-			DisplayName: tempDeveloperDetails.UserName,
-			Status: tempDeveloperDetails.Status,
-			CreatedAt: tempDeveloperDetails.CreatedAt,
-			CreatedBy: tempDeveloperDetails.CreatedBy,
-			LastmodifiedAt: tempDeveloperDetails.LastmodifiedAt,
-			LastmodifiedBy: tempDeveloperDetails.LastmodifiedBy,
-		}
-		companyDetails.Attributes = developerAttributes
-	}
-
-	resp := VerifyApiKeySuccessResponse{
-		ClientId: clientIdDetails,
-		Organization: organizationName,
-		Environment: resEnv.String,
-		Developer:   finalDeveloperDetails,
-		Company:     companyDetails,
-		App:         appDetails,
-		ApiProduct:  apiProductDetails,
-		// Identifier of the authorization code. This will be unique for each request.
-		Identifier: key, // TODO : what is this ?????
-		Kind:       "Collection", // TODO : what is this ????
-
-	}
-
-	return json.Marshal(resp)
-}
-func getKmsAttributes(db apid.DB, tenantId string, entityId string) []Attribute {
-	sql := "select name, value, type from kms_attributes where tenant_id = $1 and entity_id = $2"
-	attributesForQuery := []Attribute{}
-	attributes, err := db.Query(sql, tenantId, entityId)
-
-	if err != nil {
-		log.Error("Error while fetching attributes for tenant id : %s and entityId : %s", tenantId, entityId, err)
-		return attributesForQuery
-	}
-	for attributes.Next() {
-		att := Attribute{}
-		attributes.Scan(
-			&att.Name,
-			&att.Value,
-		)
-		if att.Name != "" {
-			attributesForQuery = append(attributesForQuery, att)
-		}
-	}
-	return attributesForQuery
-}
-
-func errorResponse(reason, errorCode string) ([]byte, error) {
-	if errorCode == "SEARCH_INTERNAL_ERROR" {
-		log.Error(reason)
-	} else {
-		log.Debug(reason)
-	}
-	resp := ErrorResponse{
-		ResponseCode:    errorCode,
-		ResponseMessage: reason,
-	}
-	return json.Marshal(resp)
+	return err
 }
