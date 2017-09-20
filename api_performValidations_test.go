54bc801e08f08b1afc6f66570373612134cd3143
Rajanish GJ <rajanishgj@google.com>
2017-09-20 16:19:47 -0700

    migrate GitHub repository names (#22)

--- api_performValidations_test.go
+++ api_performValidations_test.go
@@ -16,8 +16,8 @@ package apidVerifyApiKey
 
 import (
 	"encoding/json"
-	"github.com/30x/apid-core"
-	"github.com/30x/apid-core/factory"
+	"github.com/apid/apid-core"
+	"github.com/apid/apid-core/factory"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
